ceed1918ff84d2a8ae8ae20d29d724a66be40c70
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-04-10 12:34:24 +0200

    .

--- static/logic.py
+++ static/logic.py
@@ -1,4 +1,10 @@
-from browser import document as D
+from browser import html as H, document as D, ajax as A
+
+def q(req):
+    for  e in req.json:
+        D["x1"] <= H.B(e)
+
+A.get("/data", oncomplete=q)
 
 D["x1"] <= "Hello cica"
 
--- x.py
+++ x.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template as RT  # type: ignore
 app = Flask(__name__, template_folder="static", static_folder="static")
 
 @app.route("/")
-def hello_world():
+def root():
     return RT("index.html", data=[1, 2, 3, 4, 5])
+
+@app.route("/data")
+def getdata():
+    return [4, 5, 6, 7, 8, 1, 2]
